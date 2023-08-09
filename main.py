0b8008f0c0c44cc7b268116b039672a4873791b6
Omer Siman Tov <73315088+omersimantov@users.noreply.github.com>
2023-08-09 13:19:20 +0300

    GPT-4 & other improvements

--- main.py
+++ main.py
@@ -1,60 +1,59 @@
 import os
 import time
 import pinecone
+from PIL import Image
 import streamlit as st
-from pinecone import Index
 from dotenv import load_dotenv
-from langchain.llms import OpenAI
-from langchain.chains import RetrievalQA
 from langchain.vectorstores import Pinecone
+from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 
-# Credentials
+# Load environment variables
 load_dotenv()
+TITLE = os.getenv("COMPANY_NAME") + " GPT"
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
 PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT")
 
-# LLM
-llm = OpenAI(streaming=True, temperature=0)
+# Create Streamlit app
+favicon = Image.open("favicon.png")
+st.set_page_config(page_title=TITLE, page_icon=favicon)
+st.title(TITLE)
+if "conversation" not in st.session_state:
+    st.session_state.conversation = None
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = None
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-# Vector store
+# Configure LLM
+llm = ChatOpenAI(model="gpt-4", streaming=True, temperature=0)
+
+# Initialize vector store
 pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
-index = Index(PINECONE_INDEX_NAME)
-
-# Retriever
+index = pinecone.Index(PINECONE_INDEX_NAME)
 embedding_function = OpenAIEmbeddings().embed_query
 vectorstore = Pinecone(index, embedding_function, "text")
-qa = RetrievalQA.from_chain_type(
-    llm,
+
+# Configure retrieval chain
+chain = ConversationalRetrievalChain.from_llm(
+    llm=llm,
     chain_type="stuff",
     retriever=vectorstore.as_retriever(),
     memory=ConversationBufferMemory(memory_key="chat_history"),
     verbose=True,
 )
 
-# Streamlit app
-TITLE = os.getenv("COMPANY_NAME") + " GPT"
-st.set_page_config(page_title=TITLE)
-st.title(TITLE)
-
-if "conversation" not in st.session_state:
-    st.session_state.conversation = None
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = None
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-
+# Handle prompt
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     with st.chat_message("assistant"):
-        response = qa.run(prompt)
+        response = chain.run(prompt)
         stream = ""
         message_placeholder = st.empty()
         for chunk in response.split():
